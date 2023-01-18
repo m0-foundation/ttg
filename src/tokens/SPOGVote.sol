72d99614c25f3683d3fb4d3097aa6f54faf3252e
TheThingDev <the_thing_dev@proton.me>
2023-01-18 23:24:14 +0100

    Refactor SPOGVote

--- src/tokens/SPOGVote.sol
+++ src/tokens/SPOGVote.sol
@@ -1,44 +1,61 @@
 // SPDX-License-Identifier: GLP-3.0
 
-pragma solidity 0.8.14;
+pragma solidity 0.8.17;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Votes} from "@openzeppelin/contracts/governance/utils/Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "../interfaces/ISPOGVote.sol";
+
+import {ISPOGVote} from "../interfaces/ISPOGVote.sol";
 
 /// @title SPOGVote: token used to vote on a SPOG
-contract SPOGVote is
-    ERC20,
-    Votes,
-    ERC20Burnable,
-    AccessControlEnumerable,
-    ISPOGVote
-{
-    bytes32 public constant SPOG = keccak256("SPOG");
-
-    /// @dev A mapping of frozen accounts
-    mapping(address => bool) public isAccountFrozen;
+contract SPOGVote is ERC20Votes, ISPOGVote, AccessControlEnumerable {
+    bytes32 public constant SPOG_ROLE = keccak256("SPOG_ROLE");
 
     /// @dev Initializes the contract by creating the token as well as the default admin role and the SPOG role
     /// @param name The name of the token
     /// @param symbol The symbol of the token
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+        ERC20Permit(name)
+    {
+        // TODO: add require for msg.sender to be the SPOG
         grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        grantRole(SPOG, _msgSender()); // TODO: should be revised to add a SPOG contract as the SPOG role
+        grantRole(SPOG_ROLE, _msgSender());
     }
 
     /// @dev Restricts minting to the SPOG. Cannot mint more than the max cap
     /// @param to The address to mint to
     /// @param amount The amount to mint
-    function mint(address to, uint256 amount) public override onlyRole(SPOG) {
-        require(totalSupply() + amount <= MAX_CAP, "WTR: cap exceeded");
+    function mint(address to, uint256 amount) public onlyRole(SPOG_ROLE) {
         _mint(to, amount);
     }
 
-    /// @dev override totalSupply to return the total supply of the token in the voting contract
-    function totalSupply() public view override returns (uint256) {
-        return _getTotalSupply();
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+
+    function getChainId() external view returns (uint256) {
+        return block.chainid;
     }
 }
