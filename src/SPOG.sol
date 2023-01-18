c62fb4ca076851e836e4bcfd1add1f74653a1f70
TheThingDev <the_thing_dev@proton.me>
2023-01-18 23:28:50 +0100

    Add GovSPOG to SPOG contract

--- src/SPOG.sol
+++ src/SPOG.sol
@@ -5,6 +5,8 @@ import {IList} from "./interfaces/IList.sol";
 
 import {ISPOGVote} from "./interfaces/ISPOGVote.sol";
 
+import {GovSPOG} from "./GovSPOG.sol";
+
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -14,26 +16,42 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
  * @dev Reference: https://github.com/TheThing0/SPOG-Spec/blob/main/README.md
  * @notice A SPOG, "Simple Participation Optimized Governance," is a governance mechanism that uses token voting to maintain lists and manage communal property. As its name implies, it primarily optimizes for token holder participation. A SPOG is primarily used for **permissioning actors** and should not be used for funding/financing decisions.
  */
-contract SPOG {
+contract SPOG is GovSPOG {
     using SafeERC20 for IERC20;
 
+    struct SPOGData {
+        uint256 tax;
+        uint256 currentEpoch;
+        uint256 currentEpochEnd;
+        uint256 valueQuorum;
+        uint256 inflatorTime;
+        uint256 sellTime;
+        uint256 forkTime;
+        uint256 inflator;
+        uint256 reward;
+        uint256[2] taxRange;
+        IERC20 cash;
+    }
+    SPOGData public spogData;
+
     // contract variables
-    IERC20 public cash;
-    uint256 public taxRange;
-    uint256 public inflator;
-    uint256 public reward;
-    uint256 public voteTime;
-    uint256 public inflatorTime;
-    uint256 public sellTime;
-    uint256 public forkTime;
-    uint256 public voteQuorum;
-    uint256 public valueQuorum;
-    uint256 public tax;
+    // IERC20 public cash;
+    // uint256 public taxRange;
+    // uint256 public inflator;
+    // uint256 public reward;
+    // uint256 public inflatorTime;
+    // uint256 public sellTime;
+    // uint256 public forkTime;
+    // uint256 public valueQuorum;
+    // uint256 public tax;
 
-    uint256 public currentEpoch;
-    uint256 public currentEpochEnd;
+    // uint256 public currentEpoch;
+    // uint256 public currentEpochEnd;
 
-    ISPOGVote public vote;
+    // These are set in GovSPOG
+    // uint256 public voteQuorum;
+    // ISPOGVote public vote;
+    // uint256 public voteTime;
 
     // List of addresses that are part of the masterlist
     mapping(address => bool) public masterlist;
@@ -70,63 +88,84 @@ contract SPOG {
         uint256 _valueQuorum,
         uint256 _tax,
         ISPOGVote _vote
-    ) {
-        cash = IERC20(_cash);
-        taxRange = _taxRange[0];
-        inflator = _inflator;
-        reward = _reward;
-        voteTime = _voteTime;
-        inflatorTime = _inflatorTime;
-        sellTime = _sellTime;
-        forkTime = _forkTime;
-        voteQuorum = _voteQuorum;
-        valueQuorum = _valueQuorum;
-        tax = _tax;
-        vote = _vote;
+    ) GovSPOG(_vote, _voteQuorum, _voteTime) {
+        spogData.cash = IERC20(_cash);
+        spogData.taxRange[0] = _taxRange[0];
+        spogData.taxRange[1] = _taxRange[1];
+        spogData.inflator = _inflator;
+        spogData.reward = _reward;
+        spogData.inflatorTime = _inflatorTime;
+        spogData.sellTime = _sellTime;
+        spogData.forkTime = _forkTime;
+        spogData.valueQuorum = _valueQuorum;
+        spogData.tax = _tax;
 
-        currentEpoch = 1;
-        currentEpochEnd = block.timestamp + voteTime;
+        // These are set in GovSPOG
+        // voteTime = _voteTime;
+        // voteQuorum = _voteQuorum;
+        // vote = _vote;
+
+        spogData.currentEpoch = 1;
+        spogData.currentEpochEnd = block.timestamp + voteTime;
     }
 
     // functions for adding lists to masterlist and appending/removing addresses to/from lists through VOTE
 
     /// @notice Add a new list to the master list of the SPOG
-    /// @param _id The ID as the list address of the list to be added
-    function newList(address _id) external {
-        _pay(tax);
+    /// @param _proposalId The ID of the proposal
+    /// @param _listId The ID as the list address of the list to be added
+    function newList(uint256 _proposalId, address _listId)
+        external
+        onlyGovernance
+    {
+        _pay(spogData.tax);
 
         // require that the list is not already on the master list
-        require(!masterlist[_id], "List is already on the master list");
+        require(!masterlist[_listId], "List is already on the master list");
 
-        // TODO: require if vote has been passed in the preceding epoch
+        // check quorum reached for proposal
+        _voteSucceeded(_proposalId);
 
         // add the list to the master list
-        masterlist[_id] = true;
-        emit NewListAdded(_id);
+        masterlist[_listId] = true;
+        emit NewListAdded(_listId);
     }
 
     // create function to remove a list from the master list of the SPOG
     /// @notice Remove a list from the master list of the SPOG
-    /// @param _id The ID as the list address of the list to be removed
-    function removeList(address _id) external {
-        _pay(tax);
+    /// @param _proposalId The ID of the proposal
+    /// @param _listId The ID as the list address of the list to be removed
+    function removeList(uint256 _proposalId, address _listId)
+        external
+        onlyGovernance
+    {
+        _pay(spogData.tax);
 
         // require that the list is on the master list
-        require(masterlist[_id], "List is not on the master list");
+        require(masterlist[_listId], "List is not on the master list");
 
-        // TODO: require if vote has been passed in the preceding epoch
+        // check quorum reached for proposal
+        require(
+            _voteSucceeded(_proposalId),
+            "Vote quorum has not been reached for this action"
+        );
 
         // remove the list from the master list
-        masterlist[_id] = false;
-        emit ListRemoved(_id);
+        masterlist[_listId] = false;
+        emit ListRemoved(_listId);
     }
 
     // create function to append an address to a list
     /// @notice Append an address to a list
+    /// @param _proposalId The ID of the proposal
     /// @param _address The address to be appended to the list
     /// @param _list The list to which the address will be appended
-    function append(address _address, IList _list) external {
-        _pay(tax);
+    function append(
+        uint256 _proposalId,
+        address _address,
+        IList _list
+    ) external onlyGovernance {
+        _pay(spogData.tax);
 
         // require that the list is on the master list
         require(masterlist[address(_list)], "List is not on the master list");
@@ -134,7 +173,11 @@ contract SPOG {
         // require that the address is not already on the list
         require(!_list.contains(_address), "Address is already on the list");
 
-        // TODO: require if vote has been passed in the preceding epoch
+        // check quorum reached for proposal
+        require(
+            _voteSucceeded(_proposalId),
+            "Vote quorum has not been reached for this action"
+        );
 
         // append the address to the list
         _list.add(_address);
@@ -143,10 +186,15 @@ contract SPOG {
 
     // create function to remove an address from a list
     /// @notice Remove an address from a list
+    /// @param _proposalId The ID of the proposal
     /// @param _address The address to be removed from the list
     /// @param _list The list from which the address will be removed
-    function remove(address _address, IList _list) external {
-        _pay(tax);
+    function remove(
+        uint256 _proposalId,
+        address _address,
+        IList _list
+    ) external onlyGovernance {
+        _pay(spogData.tax);
 
         // require that the list is on the master list
         require(masterlist[address(_list)], "List is not on the master list");
@@ -154,7 +202,11 @@ contract SPOG {
         // require that the address is on the list
         require(_list.contains(_address), "Address is not on the list");
 
-        // TODO: require if vote has been passed in the preceding epoch
+        // check quorum reached for proposal
+        require(
+            _voteSucceeded(_proposalId),
+            "Vote quorum has not been reached for this action"
+        );
 
         // remove the address from the list
         _list.remove(_address);
@@ -163,10 +215,15 @@ contract SPOG {
 
     // create function to remove an address from a list immediately upon reaching a `VOTE QUORUM`
     /// @notice Remove an address from a list immediately upon reaching a `VOTE QUORUM`
+    /// @param _proposalId The ID of the proposal
     /// @param _address The address to be removed from the list
     /// @param _list The list from which the address will be removed
-    function emergencyRemove(address _address, IList _list) external {
-        _pay(tax * 12);
+    function emergencyRemove(
+        uint256 _proposalId,
+        address _address,
+        IList _list
+    ) external onlyGovernance {
+        _pay(spogData.tax * 12);
 
         // require that the list is on the master list
         require(masterlist[address(_list)], "List is not on the master list");
@@ -176,7 +233,7 @@ contract SPOG {
 
         // require that the vote quorum has been reached
         require(
-            voteQuorumReached(), // implement vote quorum reached function
+            _voteSucceeded(_proposalId),
             "Vote quorum has not been reached for this action"
         );
 
@@ -185,28 +242,17 @@ contract SPOG {
         emit AddressRemovedFromList(address(_list), _address);
     }
 
-    // create function to check if the vote quorum has been reached
-    /// @notice Check if the vote quorum has been reached
-    /// @return `true` if the vote quorum has been reached, `false` otherwise
-    function voteQuorumReached() public view returns (bool) {
-        // get the number of votes cast
-        uint256 votesCast = votes.length; // TODO: implement votes count
-
-        // get the number of votes required to reach the vote quorum
-        uint256 votesRequired = (voteQuorum * vote.totalSupply()) / 100;
-
-        // return whether the vote quorum has been reached
-        return votesCast >= votesRequired;
-    }
-
     // ********** PRIVATE FUNCTIONS ********** //
 
     /// @notice pay tax from the caller to the SPOG
     /// @param _amount The amount to be transferred
     function _pay(uint256 _amount) private {
         // require that the caller pays the tax
-        require(_amount == tax, "Caller must pay tax to call this function");
+        require(
+            _amount == spogData.tax,
+            "Caller must pay tax to call this function"
+        );
         // transfer the amount from the caller to the SPOG
-        cash.safeTransferFrom(msg.sender, address(this), _amount);
+        spogData.cash.safeTransferFrom(msg.sender, address(this), _amount);
     }
 }
