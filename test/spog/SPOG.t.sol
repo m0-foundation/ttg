76883ebea586640fd51b5b50ff491cf21296083a
TheThingDev <the_thing_dev@proton.me>
2023-03-17 20:45:24 +0100

    add test for double quorum on cash var change

--- test/spog/SPOG.t.sol
+++ test/spog/SPOG.t.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.17;
 
 import "forge-std/console.sol";
+import {ERC20GodMode} from "test/mock/ERC20GodMode.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {BaseTest} from "test/Base.t.sol";
 import {SPOGDeployScript} from "script/SPOGDeploy.s.sol";
 import "src/SPOG.sol";
@@ -555,6 +557,7 @@ contract SPOGTest is BaseTest {
     function test_Revert_FileVarInSpogDataMustBeProposedByVoterHoldersFirst()
         public
     {
+        bytes32 reward = "reward";
         bytes memory elevenAsCalldataValue = abi.encode(11);
 
         uint8 yesVote = 1;
@@ -568,7 +571,7 @@ contract SPOGTest is BaseTest {
 
         calldatasForValueHolders[0] = abi.encodeWithSignature(
             "fileVarInSpogDataWithDoubleQuorum(bytes32,bytes)",
-            "reward",
+            reward,
             elevenAsCalldataValue
         );
         string
@@ -620,6 +623,7 @@ contract SPOGTest is BaseTest {
     function test_FileVarInSpogData_SPOGProposalToChangeVariableInSpog()
         public
     {
+        bytes32 reward = "reward";
         bytes memory elevenAsCalldataValue = abi.encode(11);
         uint8 yesVote = 1;
 
@@ -629,9 +633,10 @@ contract SPOGTest is BaseTest {
         uint256[] memory values = new uint256[](1);
         values[0] = 0;
         bytes[] memory calldatas = new bytes[](1);
+
         calldatas[0] = abi.encodeWithSignature(
             "fileVarInSpogDataWithDoubleQuorum(bytes32,bytes)",
-            "reward",
+            reward,
             elevenAsCalldataValue
         );
         string memory description = "Change reward variable in spog";
@@ -665,18 +670,15 @@ contract SPOGTest is BaseTest {
         // fast forward to end of voting period
         vm.roll(block.number + deployScript.voteTime() + 1);
 
-        // bytes32 reward = bytes32("reward");
-        // bytes32 identifier = keccak256(
-        //     abi.encodePacked(reward, elevenAsCalldataValue)
-        // );
+        bytes32 identifier = keccak256(
+            abi.encodePacked(reward, elevenAsCalldataValue)
+        );
         // check that DoubleQuorumInitiated event was triggered
-        // expectEmit();
-        // emit DoubleQuorumInitiated(identifier);
-        // execute proposal
+        expectEmit();
+        emit DoubleQuorumInitiated(identifier);
         govSPOGVote.execute(targets, values, calldatas, hashedDescription);
 
         (, , , , uint256 rewardFirstCheck, ) = spog.spogData();
-        console.log("rewardFirstCheck", rewardFirstCheck);
 
         // assert that reward has not been changed yet as it needs to be voted on again by value holders
         assertFalse(
@@ -698,7 +700,11 @@ contract SPOGTest is BaseTest {
                 description
             );
 
-        // vote on proposal
+        // must update start of next voting period so as to not revert on votingDelay() check
+        while (block.number >= govSPOGValue.startOfNextVotingPeriod()) {
+            govSPOGValue.updateStartOfNextVotingPeriod();
+        }
+
         deployScript.cash().approve(address(spog), deployScript.tax());
         spog.propose(
             IGovSPOG(address(govSPOGValue)),
@@ -714,9 +720,9 @@ contract SPOGTest is BaseTest {
         // fast forward to end of govSPOGValue voting period
         vm.roll(block.number + deployScript.forkTime() + 1);
 
-        // // check that DoubleQuorumFinalized event was triggered
-        // expectEmit();
-        // emit DoubleQuorumFinalized("reward", elevenAsCalldataValue);
+        // check that DoubleQuorumFinalized event was triggered
+        expectEmit();
+        emit DoubleQuorumFinalized(identifier);
         govSPOGValue.execute(
             targets,
             values,
@@ -727,5 +733,130 @@ contract SPOGTest is BaseTest {
         (, , , , uint256 rewardSecondCheck, ) = spog.spogData();
         // assert that reward was modified by double quorum
         assertTrue(rewardSecondCheck == 11, "Reward was not changed");
+    }
+
+    function test_FileVarInSpogData_ChangeCashToken_SPOGProposalToChangeVariableInSpog()
+        public
+    {
+        ERC20GodMode newCashInstance = new ERC20GodMode(
+            "New Cash",
+            "NCASH",
+            18
+        );
+        bytes32 cash = "cash";
+        bytes memory newCash = abi.encode(address(newCashInstance));
+        uint8 yesVote = 1;
+
+        // create proposal to change variable in spog
+        address[] memory targets = new address[](1);
+        targets[0] = address(spog);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+
+        calldatas[0] = abi.encodeWithSignature(
+            "fileVarInSpogDataWithDoubleQuorum(bytes32,bytes)",
+            cash,
+            newCash
+        );
+        string memory description = "Change cash variable in spog";
+
+        (
+            bytes32 hashedDescription,
+            uint256 proposalId
+        ) = getProposalIdAndHashedDescription(
+                govSPOGVote,
+                targets,
+                values,
+                calldatas,
+                description
+            );
+
+        // vote on proposal
+        deployScript.cash().approve(address(spog), deployScript.tax());
+        spog.propose(
+            IGovSPOG(address(govSPOGVote)),
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        // fast forward to an active voting period
+        vm.roll(block.number + govSPOGVote.votingDelay() + 1);
+
+        // cast vote on proposal
+        govSPOGVote.castVote(proposalId, yesVote);
+        // fast forward to end of voting period
+        vm.roll(block.number + deployScript.voteTime() + 1);
+
+        bytes32 identifier = keccak256(abi.encodePacked(cash, newCash));
+        // check that DoubleQuorumInitiated event was triggered
+        expectEmit();
+        emit DoubleQuorumInitiated(identifier);
+        govSPOGVote.execute(targets, values, calldatas, hashedDescription);
+
+        (, , , , , IERC20 cashFirstCheck) = spog.spogData();
+
+        // assert that cash has not been changed yet as it needs to be voted on again by value holders
+        assertFalse(
+            address(cashFirstCheck) == address(newCashInstance),
+            "Reward should not have been changed"
+        );
+        assertTrue(
+            address(cashFirstCheck) == address(deployScript.cash()),
+            "Cash was changed"
+        );
+
+        /**********  value holders vote on proposal **********/
+        vm.warp(1 hours);
+
+        (
+            bytes32 hashedDescriptionForValueHolders,
+            uint256 proposalIdForValueHolders
+        ) = getProposalIdAndHashedDescription(
+                govSPOGValue,
+                targets,
+                values,
+                calldatas,
+                description
+            );
+
+        // must update start of next voting period so as to not revert on votingDelay() check
+        while (block.number >= govSPOGValue.startOfNextVotingPeriod()) {
+            govSPOGValue.updateStartOfNextVotingPeriod();
+        }
+
+        deployScript.cash().approve(address(spog), deployScript.tax());
+        spog.propose(
+            IGovSPOG(address(govSPOGValue)),
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        vm.roll(block.number + govSPOGValue.votingDelay() + 1);
+
+        govSPOGValue.castVote(proposalIdForValueHolders, yesVote);
+        // fast forward to end of govSPOGValue voting period
+        vm.roll(block.number + deployScript.forkTime() + 1);
+
+        // check that DoubleQuorumFinalized event was triggered
+        expectEmit();
+        emit DoubleQuorumFinalized(identifier);
+        govSPOGValue.execute(
+            targets,
+            values,
+            calldatas,
+            hashedDescriptionForValueHolders
+        );
+
+        (, , , , , IERC20 cashSecondCheck) = spog.spogData();
+        // assert that cash was modified by double quorum
+        assertTrue(
+            address(cashSecondCheck) == address(newCashInstance),
+            "Reward was not changed"
+        );
     }
 }
