72fba02bafe0a04bccf083de17c924d259a526ec
TheThingDev <the_thing_dev@proton.me>
2023-03-28 17:00:17 +0200

    move events to interface

--- src/interfaces/ISPOG.sol
+++ src/interfaces/ISPOG.sol
@@ -4,6 +4,18 @@ pragma solidity 0.8.17;
 import {IList} from "src/interfaces/IList.sol";
 
 interface ISPOG {
+    // Events
+    event NewListAdded(address _list);
+    event ListRemoved(address _list);
+    event AddressAppendedToList(address _list, address _address);
+    event AddressRemovedFromList(address _list, address _address);
+    event NewProposal(uint256 indexed proposalId);
+    event TaxChanged(uint256 indexed tax);
+    event DoubleQuorumInitiated(bytes32 indexed identifier);
+    event DoubleQuorumFinalized(bytes32 indexed identifier);
+    error InvalidParameter(bytes32 what);
+
+    // functions
     function vault() external view returns (address);
 
     function addNewList(IList list) external;
