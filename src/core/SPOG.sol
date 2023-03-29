cda1fee3e15ca6448bf34430bacac66550074b2a
TheThingDev <the_thing_dev@proton.me>
2023-03-29 14:16:25 +0200

    fxi tests

--- src/core/SPOG.sol
+++ src/core/SPOG.sol
@@ -1,13 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.17;
 
-import {SPOGSettable} from "src/core/SPOGSettable.sol";
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IGovSPOG} from "src/interfaces/IGovSPOG.sol";
-
+import {SPOGStorage, IGovSPOG, IERC20, ISPOG} from "src/core/SPOGStorage.sol";
 import {IList} from "src/interfaces/IList.sol";
-import {ISPOG} from "src/interfaces/ISPOG.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
@@ -16,7 +11,7 @@ import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap
 /// @dev Contracts for governing lists and managing communal property through token voting.
 /// @dev Reference: https://github.com/TheThing0/SPOG-Spec/blob/main/README.md
 /// @notice A SPOG, "Simple Participation Optimized Governance," is a governance mechanism that uses token voting to maintain lists and manage communal property. As its name implies, it primarily optimizes for token holder participation. A SPOG is primarily used for **permissioning actors** and should not be used for funding/financing decisions.
-contract SPOG is ISPOG, SPOGSettable, ERC165 {
+contract SPOG is SPOGStorage, ERC165 {
     using SafeERC20 for IERC20;
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
@@ -45,16 +40,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
         uint256 _valueQuorum,
         IGovSPOG _govSPOGVote,
         IGovSPOG _govSPOGValue
-    )
-        SPOGSettable(
-            _govSPOGVote,
-            _govSPOGValue,
-            _voteTime,
-            _forkTime,
-            _voteQuorum,
-            _valueQuorum
-        )
-    {
+    ) SPOGStorage(_govSPOGVote, _govSPOGValue, _voteTime, _forkTime, _voteQuorum, _valueQuorum) {
         // TODO: add require statements for variables
         vault = _vault;
 
@@ -78,18 +64,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
             uint256 _inflatorTime,
             uint256 _sellTime,
             uint256 _tax
-        ) = abi.decode(
-                _initSPOGData,
-                (
-                    address,
-                    uint256[2],
-                    uint256,
-                    uint256,
-                    uint256,
-                    uint256,
-                    uint256
-                )
-            );
+        ) = abi.decode(_initSPOGData, (address, uint256[2], uint256, uint256, uint256, uint256, uint256));
 
         spogData = SPOGData({
             cash: IERC20(_cash),
@@ -124,10 +99,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
     /// @param list  The list address of the list to be removed
     function removeList(IList list) external onlyGovSPOGVote {
         // require that the list is on the master list
-        require(
-            masterlist.contains(address(list)),
-            "List is not on the master list"
-        );
+        require(masterlist.contains(address(list)), "List is not on the master list");
 
         // remove the list from the master list
         masterlist.remove(address(list));
@@ -139,10 +111,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
     /// @param _list The list to which the address will be appended
     function append(address _address, IList _list) external onlyGovSPOGVote {
         // require that the list is on the master list
-        require(
-            masterlist.contains(address(_list)),
-            "List is not on the master list"
-        );
+        require(masterlist.contains(address(_list)), "List is not on the master list");
 
         // require that the address is not already on the list
         require(!_list.contains(_address), "Address is already on the list");
@@ -158,10 +127,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
     /// @param _list The list from which the address will be removed
     function remove(address _address, IList _list) external onlyGovSPOGVote {
         // require that the list is on the master list
-        require(
-            masterlist.contains(address(_list)),
-            "List is not on the master list"
-        );
+        require(masterlist.contains(address(_list)), "List is not on the master list");
 
         // require that the address is on the list
         require(_list.contains(_address), "Address is not on the list");
@@ -175,10 +141,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
     /// @notice Remove an address from a list immediately upon reaching a `VOTE QUORUM`
     /// @param _address The address to be removed from the list
     /// @param _list The list from which the address will be removed
-    function emergencyRemove(
-        address _address,
-        IList _list
-    ) external onlyGovSPOGVote {
+    function emergencyRemove(address _address, IList _list) external onlyGovSPOGVote {
         _pay(spogData.tax * 12);
 
         address[] memory targets = new address[](1);
@@ -187,32 +150,19 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
 
         targets[0] = address(this);
         values[0] = 0;
-        calldatas[0] = abi.encodeWithSignature(
-            "remove(address,IList)",
-            _address,
-            _list
-        );
+        calldatas[0] = abi.encodeWithSignature("remove(address,IList)", _address, _list);
 
         string memory description = "Emergency Remove address from list";
 
-        uint256 proposalId = govSPOGVote.propose(
-            targets,
-            values,
-            calldatas,
-            description
-        );
+        uint256 proposalId = govSPOGVote.propose(targets, values, calldatas, description);
 
         emit NewProposal(proposalId);
     }
 
     /// @dev check SPOG interface support
     /// @param interfaceId The interface ID to check
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override returns (bool) {
-        return
-            interfaceId == type(ISPOG).interfaceId ||
-            super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
+        return interfaceId == type(ISPOG).interfaceId || super.supportsInterface(interfaceId);
     }
 
     /// @notice Create a new proposal
@@ -233,12 +183,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
         // require that the caller pays the tax to propose
         _pay(spogData.tax); // TODO: check for tax for emergency remove proposals
 
-        uint256 proposalId = govSPOG.propose(
-            targets,
-            values,
-            calldatas,
-            description
-        );
+        uint256 proposalId = govSPOG.propose(targets, values, calldatas, description);
         emit NewProposal(proposalId);
 
         return proposalId;
@@ -246,8 +191,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
 
     function tokenInflationCalculation() public view returns (uint256) {
         if (msg.sender == address(govSPOGVote)) {
-            uint256 votingTokenTotalSupply = IERC20(govSPOGVote.votingToken())
-                .totalSupply();
+            uint256 votingTokenTotalSupply = IERC20(govSPOGVote.votingToken()).totalSupply();
             uint256 inflator = spogData.inflator;
 
             return (votingTokenTotalSupply * inflator) / 100;
@@ -262,10 +206,7 @@ contract SPOG is ISPOG, SPOGSettable, ERC165 {
     /// @param _amount The amount to be transferred
     function _pay(uint256 _amount) private {
         // require that the caller pays the tax
-        require(
-            _amount >= spogData.tax,
-            "Caller must pay tax to call this function"
-        );
+        require(_amount >= spogData.tax, "Caller must pay tax to call this function");
         // transfer the amount from the caller to the SPOG
         spogData.cash.safeTransferFrom(msg.sender, address(this), _amount);
     }
