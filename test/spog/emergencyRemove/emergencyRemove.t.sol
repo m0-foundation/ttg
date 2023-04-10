a83e5b08120b62fb62c13b8dc3b7dc1a248f7b90
toni <antonina.norair@gmail.com>
2023-04-10 15:51:59 -0400

    Add more tests

--- test/spog/emergencyRemove/emergencyRemove.t.sol
+++ test/spog/emergencyRemove/emergencyRemove.t.sol
@@ -52,12 +52,60 @@ contract SPOG_emergencyRemove is SPOG_Base {
         return (proposalId, targets, values, calldatas, hashedDescription);
     }
 
-    // function test_Revert_ChangeTaxWhenNotCalledFromGovernance() public {
-    //     vm.expectRevert("SPOG: Only vote governor");
-    //     spog.changeTax(1);
-    // }
+    function test_Revert_EmergencyRemove_WhenNotEnoughTaxPaid() public {
+        address[] memory targets = new address[](1);
+        targets[0] = address(spog);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("emergencyRemove(address,address)", addressToRemove, address(list));
+        string memory description = "Emergency remove of merchant";
 
-    function test_EmergencyRemoveBeforeDeadline() public {
+        // emergency propose, 12 * tax price is needed, but 1 * tax is allowed to be paid
+        deployScript.cash().approve(address(spog), deployScript.tax());
+        vm.expectRevert("ERC20: insufficient allowance");
+        spog.propose(targets, values, calldatas, description);
+    }
+
+    function test_Revert_EmergencyRemove_WhenQuorumWasNotReached() public {
+        // create proposal to emergency remove address from list
+        (
+            uint256 proposalId,
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas,
+            bytes32 hashedDescription
+        ) = createEmergencyProposal();
+
+        // Emergency proposal is in the governor list
+        assertTrue(voteGovernor.emergencyProposals(proposalId), "Proposal was added to the list");
+
+        // fast forward to an active voting period
+        vm.roll(block.number + voteGovernor.votingDelay() + 1);
+
+        // cast vote on proposal
+        voteGovernor.castVote(proposalId, noVote);
+
+        vm.expectRevert("Governor: proposal not successful");
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal is active
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Active, "Not in active state");
+
+        // fast forward to end of voting period
+        vm.roll(block.number + deployScript.voteTime() + 1);
+
+        vm.expectRevert("Governor: proposal not successful");
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal was defeated
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Defeated, "Not in defeated state");
+
+        // assert that address is in the list
+        assertTrue(list.contains(addressToRemove), "Address is not in the list");
+    }
+
+    function test_EmergencyRemove_BeforeDeadlineEnd() public {
         // create proposal to emergency remove address from list
         uint256 balanceBeforeProposal = deployScript.cash().balanceOf(address(vault));
         (
@@ -103,7 +151,7 @@ contract SPOG_emergencyRemove is SPOG_Base {
         assertFalse(list.contains(addressToRemove), "Address is still in the list");
     }
 
-    function test_EmergencyRemoveAfterDeadline() public {
+    function test_EmergencyRemove_AfterDeadlineEnd() public {
         // create proposal to emergency remove address from list
         (
             uint256 proposalId,
