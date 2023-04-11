a6f828cd298564f6d2d75ae81608a0dbba71cff5
D Midnight <53611782+dmidnight@users.noreply.github.com>
2023-04-11 14:25:45 -0400

    Allow partial buy in dutch auction

--- src/periphery/ERC20DutchAuction.sol
+++ src/periphery/ERC20DutchAuction.sol
@@ -10,70 +10,71 @@ contract DutchAuction {
     IERC20 public paymentToken;
     uint256 public auctionDuration;
     uint256 public auctionEndTime;
-    uint256 public startPrice;
-    uint256 public endPrice;
-    uint256 public auctionTokenAmount;
-    bool public auctionCompleted;
+    uint256 public ceilingPrice;
+    uint256 public floorPrice;
     address public vault;
 
-    event AuctionCompleted(address indexed winner, uint256 amount, uint256 price);
+    event AuctionPurchase(address indexed buyer, uint256 amount, uint256 price);
 
     constructor(
         IERC20 _auctionToken,
         IERC20 _paymentToken,
         uint256 _auctionDuration,
-        uint256 _startPrice,
-        uint256 _endPrice,
-        uint256 _auctionTokenAmount,
-         address _vault
+        uint256 _ceilingPrice,
+        uint256 _floorPrice,
+        address _vault
     ) {
         auctionToken = _auctionToken;
         paymentToken = _paymentToken;
         auctionDuration = _auctionDuration;
         auctionEndTime = block.timestamp + _auctionDuration;
-        startPrice = _startPrice;
-        endPrice = _endPrice;
-        auctionTokenAmount = _auctionTokenAmount;
+        ceilingPrice = _ceilingPrice;
+        floorPrice = _floorPrice;
         vault = _vault;
+    }
 
-        require(auctionToken.transferFrom(vault, address(this), auctionTokenAmount), "Token transfer failed");
+    function depositFromVault(uint256 _auctionTokenAmount) public {
+        require(auctionToken.transferFrom(vault, address(this), _auctionTokenAmount), "Token transfer failed");
     }
 
     function getCurrentPrice() public view returns (uint256) {
         if (block.timestamp >= auctionEndTime) {
-            return endPrice;
+            return floorPrice;
         }
 
         uint256 timePassed = block.timestamp - (auctionEndTime - auctionDuration);
-        uint256 priceDifference = startPrice - endPrice;
+        uint256 priceDifference = ceilingPrice - floorPrice;
         uint256 priceDrop = (priceDifference * timePassed) / auctionDuration;
 
-        return startPrice - priceDrop;
+        return ceilingPrice - priceDrop;
     }
 
-    function buyTokens() public {
-        require(!auctionCompleted, "Auction already completed");
+    function buyTokens(uint256 amountToPay) public {
+        require(auctionToken.balanceOf(address(this)) > 0, "Auction balance is zero");
         require(block.timestamp <= auctionEndTime, "Auction already ended");
 
         uint256 currentPrice = getCurrentPrice();
-        uint256 amountToPay = auctionTokenAmount * currentPrice;
-
-        auctionCompleted = true;
-
-        // Transfer the auctioned tokens to the highest bidder
-        require(auctionToken.transfer(msg.sender, auctionTokenAmount), "Token transfer failed");
+        uint256 amountToReceive = amountToPay / currentPrice;
 
         // Transfer the winning bid amount to the vault
         require(paymentToken.transferFrom(msg.sender, vault, amountToPay), "Token transfer failed");
 
-        emit AuctionCompleted(msg.sender, auctionTokenAmount, currentPrice);
+        // Transfer the auctioned tokens to the highest bidder
+        require(auctionToken.transfer(msg.sender, amountToReceive), "Token transfer failed");
+
+        emit AuctionPurchase(msg.sender, amountToReceive, currentPrice);
+    }
+
+    function buyAllTokens() public {
+        uint256 currentPrice = getCurrentPrice();
+        uint256 amountToPay = auctionToken.balanceOf(address(this)) * currentPrice;
+        
+        buyTokens(amountToPay);
     }
 
     function withdraw() public {
         require(block.timestamp > auctionEndTime, "Auction not yet ended");
-        require(!auctionCompleted, "Auction already completed");
-
-        auctionCompleted = true;
+        require(auctionToken.balanceOf(address(this)) > 0, "Auction already completed");
 
         require(auctionToken.transfer(vault, auctionToken.balanceOf(address(this))), "Token transfer failed");
     }
