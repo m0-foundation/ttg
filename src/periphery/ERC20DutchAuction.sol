b7a00a5fb4a2fb408c5fc26e3200fd428e106a73
D Midnight <53611782+dmidnight@users.noreply.github.com>
2023-04-14 17:34:57 -0400

    better desc

--- src/periphery/ERC20DutchAuction.sol
+++ src/periphery/ERC20DutchAuction.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /// @title DutchAuction
-/// @notice A contract for conducting a Dutch auction of ERC20 tokens
+/// @notice A contract for conducting a Dutch auction of ERC20 tokens without a price oracle
 contract DutchAuction {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
