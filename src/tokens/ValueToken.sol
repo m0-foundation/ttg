0c8d4787c281b4d40c195678f7ccb7bfa24f4d04
toni <antonina.norair@gmail.com>
2023-04-18 22:33:53 -0400

    Fix imports

--- src/tokens/ValueToken.sol
+++ src/tokens/ValueToken.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.17;
 
 import {ERC20Snapshot} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Votes} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 import {SPOGVotes} from "./SPOGVotes.sol";
 
