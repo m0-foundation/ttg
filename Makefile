0a3ad0b20f00c3cc08a83a1bc6582d52e7a6457d
dmidnight <53611782+dmidnight@users.noreply.github.com>
2023-05-09 07:02:45 -0400

    Anvil deploy script for local dApp dev (#72)
    Co-authored-by: TheThingDev <the_thing_dev@proton.me>

--- Makefile
+++ Makefile
@@ -5,10 +5,12 @@
 # dapp deps
 update:; forge update
 
-#coverage report
+# coverage report
 coverage :; forge coverage --report lcov && lcov --remove ./lcov.info -o ./lcov.info 'script/*' 'test/*' && genhtml lcov.info --branch-coverage --output-dir coverage
 
 # Deployment helpers
-deploy-spog-local :; forge script script/SPOGDeploy.s.sol --rpc-url localhost --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -v
+
+# deploy spog and mint cash, token and value to deployer + 5 addresses from the anvil mnemonic
+deploy-spog-local :; forge script script/LocalTestDeployScript.s.sol --rpc-url localhost --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -v
 
 deploy-spog-sepolia :; forge script script/SPOGDeploy.s.sol --rpc-url sepolia --private-key ${ETH_PK} --broadcast -vvv
---
+++ script/LocalTestDeployScript.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {BaseScript} from "script/shared/Base.s.sol";
+import {SPOGDeployScript} from "script/SPOGDeploy.s.sol";
+import {console} from "forge-std/Script.sol";
+
+contract LocalTestDeployScript is BaseScript {
+    SPOGDeployScript public spogDeployScript;
+
+    function setUp() public override {
+        super.setUp();
+
+        // deploy SPOG
+        spogDeployScript = new SPOGDeployScript();
+        spogDeployScript.run();
+    }
+
+    function run() public {
+        vm.startBroadcast(deployer);
+        // set up users
+        string memory mnemonic = "test test test test test test test test test test test junk"; // anvil accounts mnemonic
+        (address user1,) = deriveRememberKey(mnemonic, 1);
+        (address user2,) = deriveRememberKey(mnemonic, 2);
+        (address user3,) = deriveRememberKey(mnemonic, 3);
+        (address user4,) = deriveRememberKey(mnemonic, 4);
+        (address user5,) = deriveRememberKey(mnemonic, 5);
+
+        address[5] memory users = [user1, user2, user3, user4, user5];
+        // mint tokens to users
+        uint256 amount = 100000e18; // 100K
+
+        for (uint256 i = 0; i < users.length; i++) {
+            spogDeployScript.cash().mint(users[i], amount);
+            spogDeployScript.value().mint(users[i], amount);
+            spogDeployScript.vote().mint(users[i], amount);
+        }
+
+        console.log("Minted 100K cash, value, and vote to each user");
+
+        vm.stopBroadcast();
+    }
+}
