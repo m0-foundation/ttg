52f34701b148b6a8949d2f5784a6485da45a43a5
D Midnight <53611782+dmidnight@users.noreply.github.com>
2023-05-22 15:32:46 -0400

    Cover append and changeConfig

--- test/spog/emergency/emergency.t.sol
+++ test/spog/emergency/emergency.t.sol
@@ -3,10 +3,23 @@ pragma solidity 0.8.19;
 
 import "test/shared/SPOG_Base.t.sol";
 import {IGovernor} from "@openzeppelin/contracts/governance/Governor.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {ISPOG} from "src/interfaces/ISPOG.sol";
 
+interface IMockConfig {
+    function someValue() external view returns (uint256);
+}
+
+contract MockConfig is IMockConfig, ERC165 {
+    uint256 public immutable someValue = 1;
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IMockConfig).interfaceId || super.supportsInterface(interfaceId);
+    }
+}
+
 contract SPOG_emergency is SPOG_Base {
-    address internal addressToRemove;
+    address internal addressToChange;
     uint8 internal yesVote;
     uint8 internal noVote;
 
@@ -20,7 +33,7 @@ contract SPOG_emergency is SPOG_Base {
 
         // Initial state - list contains 1 merchant
         addNewListToSpogAndAppendAnAddressToIt();
-        addressToRemove = address(0x1234);
+        addressToChange = address(0x1234);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -32,10 +45,10 @@ contract SPOG_emergency is SPOG_Base {
         returns (uint256, address[] memory, uint256[] memory, bytes[] memory, bytes32)
     {
         // assert that address is in the list
-        assertTrue(list.contains(addressToRemove), "Address is not in the list");
+        assertTrue(list.contains(addressToChange), "Address is not in the list");
 
         // the actual proposal to wrap as an emergency
-        bytes memory callData = abi.encode(addressToRemove, address(list));
+        bytes memory callData = abi.encode(addressToChange, address(list));
 
         // the emergency proposal
         address[] memory targets = new address[](1);
@@ -60,8 +73,76 @@ contract SPOG_emergency is SPOG_Base {
         return (proposalId, targets, values, calldatas, hashedDescription);
     }
 
+    function createEmergencyAppendProposal()
+        internal
+        returns (uint256, address[] memory, uint256[] memory, bytes[] memory, bytes32)
+    {
+        vm.prank(address(spog));
+        list.remove(addressToChange);
+        // assert that address is not in the list
+        assertFalse(list.contains(addressToChange), "Address is in the list");
+
+        // the actual proposal to wrap as an emergency
+        bytes memory callData = abi.encode(addressToChange, address(list));
+
+        // the emergency proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(spog);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("emergency(uint8,bytes)", uint8(ISPOG.EmergencyType.Append), callData);
+        string memory description = "Emergency add of merchant";
+
+        (bytes32 hashedDescription, uint256 proposalId) =
+            getProposalIdAndHashedDescription(voteGovernor, targets, values, calldatas, description);
+
+        // emergency propose, 12 * tax price
+        deployScript.cash().approve(address(spog), spog.EMERGENCY_TAX_MULTIPLIER() * deployScript.tax());
+
+        // Check that `NewEmergencyProposal` event is emitted
+        expectEmit();
+        emit NewEmergencyProposal(proposalId);
+        spog.propose(targets, values, calldatas, description);
+
+        return (proposalId, targets, values, calldatas, hashedDescription);
+    }
+
+    function createEmergencyConfigChangeProposal()
+        internal
+        returns (uint256, address[] memory, uint256[] memory, bytes[] memory, bytes32, address)
+    {
+        MockConfig mockConfig = new MockConfig();
+
+        // the actual proposal to wrap as an emergency
+        bytes memory callData = abi.encode(keccak256("Fake Name"), address(mockConfig), type(IMockConfig).interfaceId);
+
+        // the emergency proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(spog);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] =
+            abi.encodeWithSignature("emergency(uint8,bytes)", uint8(ISPOG.EmergencyType.ChangeConfig), callData);
+        string memory description = "Emergency change config";
+
+        (bytes32 hashedDescription, uint256 proposalId) =
+            getProposalIdAndHashedDescription(voteGovernor, targets, values, calldatas, description);
+
+        // emergency propose, 12 * tax price
+        deployScript.cash().approve(address(spog), spog.EMERGENCY_TAX_MULTIPLIER() * deployScript.tax());
+
+        // Check that `NewEmergencyProposal` event is emitted
+        expectEmit();
+        emit NewEmergencyProposal(proposalId);
+        spog.propose(targets, values, calldatas, description);
+
+        return (proposalId, targets, values, calldatas, hashedDescription, address(mockConfig));
+    }
+
     function test_Revert_Emergency_WhenNotEnoughTaxPaid() public {
-        bytes memory callData = abi.encode(addressToRemove, address(list));
+        bytes memory callData = abi.encode(addressToChange, address(list));
 
         address[] memory targets = new address[](1);
         targets[0] = address(spog);
@@ -112,10 +193,10 @@ contract SPOG_emergency is SPOG_Base {
         assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Defeated, "Not in defeated state");
 
         // assert that address is in the list
-        assertTrue(list.contains(addressToRemove), "Address is not in the list");
+        assertTrue(list.contains(addressToChange), "Address is not in the list");
     }
 
-    function test_Emergency_BeforeDeadlineEnd() public {
+    function test_EmergencyRemove_BeforeDeadlineEnd() public {
         // create proposal to emergency remove address from list
         uint256 votingPeriodBeforeER = voteGovernor.votingPeriod();
         uint256 balanceBeforeProposal = deployScript.cash().balanceOf(address(valueVault));
@@ -163,10 +244,10 @@ contract SPOG_emergency is SPOG_Base {
         assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
 
         // assert that address is not in the list
-        assertFalse(list.contains(addressToRemove), "Address is still in the list");
+        assertFalse(list.contains(addressToChange), "Address is still in the list");
     }
 
-    function test_Emergency_AfterDeadlineEnd() public {
+    function test_EmergencyRemove_AfterDeadlineEnd() public {
         // create proposal to emergency remove address from list
         (
             uint256 proposalId,
@@ -194,7 +275,175 @@ contract SPOG_emergency is SPOG_Base {
         assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
 
         // assert that address is not in the list
-        assertFalse(list.contains(addressToRemove), "Address is still in the list");
+        assertFalse(list.contains(addressToChange), "Address is still in the list");
+    }
+
+    function test_EmergencyAppend_BeforeDeadlineEnd() public {
+        // create proposal to emergency remove address from list
+        uint256 votingPeriodBeforeER = voteGovernor.votingPeriod();
+        uint256 balanceBeforeProposal = deployScript.cash().balanceOf(address(valueVault));
+        (
+            uint256 proposalId,
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas,
+            bytes32 hashedDescription
+        ) = createEmergencyAppendProposal();
+
+        // Check that tax was paid
+        uint256 balanceAfterProposal = deployScript.cash().balanceOf(address(valueVault));
+        assertEq(
+            balanceAfterProposal - balanceBeforeProposal,
+            spog.EMERGENCY_TAX_MULTIPLIER() * deployScript.tax(),
+            "Emergency proposal costs 12x tax"
+        );
+
+        // Emergency proposal is in the governor list
+        assertTrue(voteGovernor.emergencyProposals(proposalId), "Proposal was added to the list");
+
+        assertEq(voteGovernor.proposalSnapshot(proposalId), block.number + 1);
+
+        // check proposal is pending
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Pending, "Not in pending state");
+
+        // fast forward to an active voting period
+        vm.roll(block.number + 2);
+
+        assertTrue(voteGovernor.votingPeriod() == votingPeriodBeforeER, "Governor voting period was messed up");
+
+        // check proposal is active
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Active, "Not in active state");
+
+        // cast vote on proposal
+        voteGovernor.castVote(proposalId, yesVote);
+
+        // check proposal is succeeded
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Succeeded, "Not in succeeded state");
+
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal was executed
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
+
+        // assert that address is in the list
+        assertTrue(list.contains(addressToChange), "Address is not in the list");
+    }
+
+    function test_EmergencyAppend_AfterDeadlineEnd() public {
+        // create proposal to emergency remove address from list
+        (
+            uint256 proposalId,
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas,
+            bytes32 hashedDescription
+        ) = createEmergencyAppendProposal();
+
+        // Emergency proposal is in the governor list
+        assertTrue(voteGovernor.emergencyProposals(proposalId), "Proposal was added to the list");
+
+        // fast forward to an active voting period
+        vm.roll(block.number + voteGovernor.votingDelay() + 1);
+
+        // cast vote on proposal
+        voteGovernor.castVote(proposalId, yesVote);
+
+        // fast forward to end of voting period
+        vm.roll(block.number + deployScript.time() + 1);
+
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal was executed
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
+
+        // assert that address is in the list
+        assertTrue(list.contains(addressToChange), "Address is not in the list");
+    }
+
+    function test_EmergencyChangeConfig_BeforeDeadlineEnd() public {
+        // create proposal to emergency remove address from list
+        uint256 votingPeriodBeforeER = voteGovernor.votingPeriod();
+        uint256 balanceBeforeProposal = deployScript.cash().balanceOf(address(valueVault));
+        (
+            uint256 proposalId,
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas,
+            bytes32 hashedDescription,
+            address configAddress
+        ) = createEmergencyConfigChangeProposal();
+
+        // Check that tax was paid
+        uint256 balanceAfterProposal = deployScript.cash().balanceOf(address(valueVault));
+        assertEq(
+            balanceAfterProposal - balanceBeforeProposal,
+            spog.EMERGENCY_TAX_MULTIPLIER() * deployScript.tax(),
+            "Emergency proposal costs 12x tax"
+        );
+
+        // Emergency proposal is in the governor list
+        assertTrue(voteGovernor.emergencyProposals(proposalId), "Proposal was added to the list");
+
+        assertEq(voteGovernor.proposalSnapshot(proposalId), block.number + 1);
+
+        // check proposal is pending
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Pending, "Not in pending state");
+
+        // fast forward to an active voting period
+        vm.roll(block.number + 2);
+
+        assertTrue(voteGovernor.votingPeriod() == votingPeriodBeforeER, "Governor voting period was messed up");
+
+        // check proposal is active
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Active, "Not in active state");
+
+        // cast vote on proposal
+        voteGovernor.castVote(proposalId, yesVote);
+
+        // check proposal is succeeded
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Succeeded, "Not in succeeded state");
+
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal was executed
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
+
+        // assert that config was changed
+        (address a,) = spog.getConfig(keccak256("Fake Name"));
+        assertEq(a, configAddress, "Config address did not match");
+    }
+
+    function test_EmergencyChangeConfig_AfterDeadlineEnd() public {
+        // create proposal to emergency remove address from list
+        (
+            uint256 proposalId,
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory calldatas,
+            bytes32 hashedDescription,
+            address configAddress
+        ) = createEmergencyConfigChangeProposal();
+
+        // Emergency proposal is in the governor list
+        assertTrue(voteGovernor.emergencyProposals(proposalId), "Proposal was added to the list");
+
+        // fast forward to an active voting period
+        vm.roll(block.number + voteGovernor.votingDelay() + 1);
+
+        // cast vote on proposal
+        voteGovernor.castVote(proposalId, yesVote);
+
+        // fast forward to end of voting period
+        vm.roll(block.number + deployScript.time() + 1);
+
+        spog.execute(targets, values, calldatas, hashedDescription);
+
+        // check proposal was executed
+        assertTrue(voteGovernor.state(proposalId) == IGovernor.ProposalState.Executed, "Not in executed state");
+
+        // assert that config was changed
+        (address a,) = spog.getConfig(keccak256("Fake Name"));
+        assertEq(a, configAddress, "Config address did not match");
     }
 
     function test_Emergency_VoteAndValueTokensAreNotInflated() public {
