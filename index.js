5c1ff772a07db86687be3a8bd87b5149aadcf927
D Midnight <53611782+dmidnight@users.noreply.github.com>
2023-06-01 12:45:48 -0400

    Update NPM module to export only what the dApp needs

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-const ERC165CheckerSPOG = require("./out/ERC165CheckerSPOG.sol/ERC165CheckerSPOG.json");
+const IERC20PricelessAuction = require("./out/IERC20PricelessAuction.sol/IERC20PricelessAuction.json");
 const IList = require("./out/IList.sol/IList.json");
-const IProtocolConfigurator = require("./out/IProtocolConfigurator.sol/IProtocolConfigurator.json");
 const ISPOG = require("./out/ISPOG.sol/ISPOG.json");
 const ISPOGGovernor = require("./out/ISPOGGovernor.sol/ISPOGGovernor.json");
-const ISPOGVotes = require("./out/ISPOGVotes.sol/ISPOGVotes.json");
 const IValueToken = require("./out/IValueToken.sol/IValueToken.json");
 const IVault = require("./out/IVault.sol/IVault.json");
 const IVoteToken = require("./out/IVoteToken.sol/IVoteToken.json");
-const List = require("./out/List.sol/List.json");
-const ListFactory = require("./out/ListFactory.sol/ListFactory.json");
-const ProtocolConfigurator = require("./out/ProtocolConfigurator.sol/ProtocolConfigurator.json");
-const SPOG = require("./out/SPOG.sol/SPOG.json");
-const SPOGDeploy = require("./out/SPOGDeploy.s.sol/SPOGDeployScript.json");
-const SPOGFactory = require("./out/SPOGFactory.sol/SPOGFactory.json");
-const SPOGGoverned = require("./out/SPOGGoverned.sol/SPOGGoverned.json");
-const SPOGGovernor = require("./out/SPOGGovernor.sol/SPOGGovernor.json");
-const SPOGGovernorFactory = require("./out/SPOGGovernorFactory.sol/SPOGGovernorFactory.json");
-const SPOGStorage = require("./out/SPOGStorage.sol/SPOGStorage.json");
-const SPOGVotes = require("./out/SPOGVotes.sol/SPOGVotes.json");
-const ValueToken = require("./out/ValueToken.sol/ValueToken.json");
-const Vault = require("./out/Vault.sol/Vault.json");
-const VoteToken = require("./out/VoteToken.sol/VoteToken.json");
 
 module.exports = {
-  SPOG: SPOG.abi,
-  ERC165CheckerSPOG: ERC165CheckerSPOG.abi,
   IList: IList.abi,
-  IProtocolConfigurator: IProtocolConfigurator.abi,
   ISPOG: ISPOG.abi,
   ISPOGGovernor: ISPOGGovernor.abi,
-  ISPOGVotes: ISPOGVotes.abi,
   IValueToken: IValueToken.abi,
   IVault: IVault.abi,
   IVoteToken: IVoteToken.abi,
-  List: List.abi,
-  ListFactory: ListFactory.abi,
-  ProtocolConfigurator: ProtocolConfigurator.abi,
-  SPOG: SPOG.abi,
-  SPOGDeploy: SPOGDeploy.abi,
-  SPOGFactory: SPOGFactory.abi,
-  SPOGGoverned: SPOGGoverned.abi,
-  SPOGGovernor: SPOGGovernor.abi,
-  SPOGGovernorFactory: SPOGGovernorFactory.abi,
-  SPOGStorage: SPOGStorage.abi,
-  SPOGVotes: SPOGVotes.abi,
-  ValueToken: ValueToken.abi,
-  Vault: Vault.abi,
-  VoteToken: VoteToken.abi,
 };
