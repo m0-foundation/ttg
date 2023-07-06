a32bff1714dd65501b9a1912d1ad1cb01bc326e5
toninorair <antonina.norair@gmail.com>
2023-07-06 14:31:21 -0400

    Add inflationRewards

--- test/vault/inflationRewards.t.sol
+++ test/vault/inflationRewards.t.sol
@@ -423,8 +423,6 @@ contract InflationRewardsTest is SPOG_Base {
         // 144 from bob, 144 from alice, 120 from carol
         // alice and carol delegated during this epoch, do they votes do not account for voting power inflation
         assertEq(vote.getVotes(bob), 408e18);
-        // uint256 bobRewards = InflationaryVotes(address(vote)).claimVoteRewards();
-        // console.log("bobRewards", bobRewards);
         vm.stopPrank();
 
         vm.startPrank(carol);
@@ -482,6 +480,121 @@ contract InflationRewardsTest is SPOG_Base {
             vote.getVotes(bob),
             vote.balanceOf(bob) + vote.balanceOf(alice) + vote.balanceOf(carol),
             "Incorrect bob votes"
+        );
+    }
+
+    function test_UsersVoteInflationForMultipleEpochsWithTransfers() public {
+        // epoch - set up proposals
+        (uint256 proposal1Id,,,,) = proposeAddingNewListToSpog("Add new list to spog 1");
+
+        /// EPOCH 1
+
+        // voting period started
+        vm.roll(block.number + governor.votingDelay() + 1);
+
+        vm.startPrank(alice);
+        // alice votes on proposal 1
+        governor.castVote(proposal1Id, yesVote);
+        assertEq(vote.getVotes(alice), 120e18);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        vote.transfer(alice, 10e18);
+        assertEq(vote.getVotes(alice), 130e18);
+        vote.transfer(carol, 10e18);
+        assertEq(vote.getVotes(carol), 110e18);
+        // bob votes on proposal 1
+        governor.castVote(proposal1Id, yesVote);
+        assertEq(vote.getVotes(bob), 96e18);
+        vm.stopPrank();
+
+        vm.startPrank(carol);
+        // carol votes on proposal 1
+        governor.castVote(proposal1Id, yesVote);
+        // carol gets 20 reward for voting, bob new voting power is not accounted for
+        assertEq(vote.getVotes(carol), 130e18, "incorrect carol votes");
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+        uint256 aliceRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(aliceRewards, 20e18, "Incorrect alice rewards");
+        assertEq(vote.balanceOf(alice), 130e18);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        uint256 bobRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(bobRewards, 16e18, "Incorrect bob rewards");
+        assertEq(vote.balanceOf(bob), 96e18);
+        vm.stopPrank();
+
+        vm.startPrank(carol);
+        uint256 carolRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(carolRewards, 20e18, "Incorrect carol rewards");
+        assertEq(vote.balanceOf(alice), 130e18);
+        vm.stopPrank();
+
+        // Main invariant of system
+        assertEq(
+            vote.getVotes(alice) + vote.getVotes(bob) + vote.getVotes(carol),
+            vote.balanceOf(alice) + vote.balanceOf(bob) + vote.balanceOf(carol),
+            "Incorrect total votes and balances"
+        );
+
+        // Atempt to claim again - no rewards
+        vm.startPrank(alice);
+        aliceRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(aliceRewards, 0, "Incorrect alice rewards");
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        bobRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(bobRewards, 0, "Incorrect bob rewards");
+        vm.stopPrank();
+
+        vm.startPrank(carol);
+        carolRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(carolRewards, 0, "Incorrect carol rewards");
+        vm.stopPrank();
+
+        // fast forward to end of voting period
+        vm.roll(block.number + governor.votingPeriod() + 1);
+
+        /// EPOCH 2
+
+        (uint256 proposal2Id,,,,) = proposeAddingNewListToSpog("Add new list to spog 2");
+
+        // voting period started
+        vm.roll(block.number + governor.votingDelay() + 1);
+
+        vm.startPrank(alice);
+        // alice votes on proposal 2
+        governor.castVote(proposal2Id, yesVote);
+        assertEq(vote.getVotes(alice), 156e18);
+        aliceRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(aliceRewards, 26e18, "Incorrect alice rewards");
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        // bob votes on proposal 2
+        governor.castVote(proposal2Id, yesVote);
+        bobRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(bobRewards, 192e17, "Incorrect bob rewards");
+        assertEq(vote.getVotes(bob), 1152e17);
+        vm.stopPrank();
+
+        vm.startPrank(carol);
+        // carol votes on proposal 2
+        governor.castVote(proposal2Id, yesVote);
+        assertEq(vote.getVotes(carol), 156e18);
+        carolRewards = InflationaryVotes(address(vote)).claimVoteRewards();
+        assertEq(carolRewards, 26e18, "Incorrect carol rewards");
+        vm.stopPrank();
+
+        // Main invariant of system
+        assertEq(
+            vote.getVotes(alice) + vote.getVotes(bob) + vote.getVotes(carol),
+            vote.balanceOf(alice) + vote.balanceOf(bob) + vote.balanceOf(carol),
+            "Incorrect total votes and balances"
         );
     }
 }
