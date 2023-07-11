fb45edcc3a0b82ff5700ea84202b5770e1ed773d
Michael De Luca <michael.deluca@circle-free.com>
2023-07-11 11:26:23 -0400

    chore: relative lib imports

--- test/interfaces/ImportedInterfaces.sol
+++ test/interfaces/ImportedInterfaces.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import { IAccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IGovernor } from "@openzeppelin/contracts/governance/IGovernor.sol";
+import { IAccessControl } from "../../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import { IERC165 } from "../../lib/openzeppelin-contracts/contracts/utils/introspection/IERC165.sol";
+import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IGovernor } from "../../lib/openzeppelin-contracts/contracts/governance/IGovernor.sol";
