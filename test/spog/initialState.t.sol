4aec959964ba6df73b64b1514d7edbd224a8f450
toninorair <antonina.norair@gmail.com>
2023-07-13 13:05:29 -0400

    revert back lines

--- test/spog/initialState.t.sol
+++ test/spog/initialState.t.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.19;
 
 import { ISPOG } from "../../src/interfaces/ISPOG.sol";
+
 import { SPOG } from "../../src/core/SPOG.sol";
+
 import { SPOGBaseTest } from "../shared/SPOGBaseTest.t.sol";
 
 contract SPOG_InitialState is SPOGBaseTest {
