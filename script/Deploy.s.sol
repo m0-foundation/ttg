808feab3305ac0af376af4c8e5f21847cb08ccdb
Michael De Luca <michael.deluca@circle-free.com>
2023-10-19 12:41:46 -0400

    fix: deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,19 +5,22 @@ pragma solidity 0.8.20;
 import { DeployBase } from "./DeployBase.s.sol";
 
 contract Deploy is DeployBase {
-    address internal constant _WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    // NOTE: Ensure this is the correct cash token address (current mainnet WETH).
+    address internal constant _CASH_TOKEN = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // WETH
 
+    // NOTE: Ensure this is the current nonce (transaction count) of the deploying address.
     uint256 internal constant _DEPLOYER_NONCE = 0;
 
-    address[] _initialPowerAccounts;
+    // NOTE: Populate these arrays with accounts and starting balances.
+    address[] _initialPowerAccounts = [address(0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD)];
 
-    uint256[] _initialPowerBalances;
+    uint256[] _initialPowerBalances = [1_000_000];
 
-    address[] _initialZeroAccounts;
+    address[] _initialZeroAccounts = [address(0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD)];
 
-    uint256[] _initialZeroBalances;
+    uint256[] _initialZeroBalances = [1_000_000];
 
-    function deploy() external {
+    function run() external {
         (address deployer_, ) = deriveRememberKey(vm.envString("MNEMONIC"), 0);
 
         deploy(
@@ -27,7 +30,7 @@ contract Deploy is DeployBase {
             _initialPowerBalances,
             _initialZeroAccounts,
             _initialZeroBalances,
-            _WETH
+            _CASH_TOKEN
         );
     }
 }
