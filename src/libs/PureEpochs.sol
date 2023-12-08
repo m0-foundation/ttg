70ff5aee7bf9bb92737047ec3e6fe95830af4365
Michael De Luca <michael.deluca@circle-free.com>
2023-12-08 16:00:56 -0500

    Sepolia testing branch

--- src/libs/PureEpochs.sol
+++ src/libs/PureEpochs.sol
@@ -6,10 +6,10 @@ pragma solidity 0.8.23;
 /// @dev    Allows for a `uint16` epoch up to timestamp 86,595,288,162 (i.e. Thu, Feb 05, Year 4714, 06:42:42 GMT).
 library PureEpochs {
     /// @notice The timestamp of The Merge block.
-    uint40 internal constant _MERGE_TIMESTAMP = 1_663_224_162;
+    uint40 internal constant _MERGE_TIMESTAMP = 1_704_809_636;
 
     /// @notice The approximate target of seconds an epoch should endure.
-    uint40 internal constant _EPOCH_PERIOD = 15 days;
+    uint40 internal constant _EPOCH_PERIOD = 400 seconds;
 
     function currentEpoch() internal view returns (uint16 currentEpoch_) {
         return uint16(((block.timestamp - _MERGE_TIMESTAMP) / _EPOCH_PERIOD) + 1); // Epoch at `_MERGE_TIMESTAMP` is 1.
