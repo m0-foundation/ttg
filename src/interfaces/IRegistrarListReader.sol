03665e54b34436d1de498e40f5aa2b9daf1af267
Jordan Hall <oighty6@gmail.com>
2025-12-11 11:56:01 -0600

    feat: read list membership for bytes32 from registrar

---
+++ src/interfaces/IRegistrarListReader.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.23;
+
+interface IRegistrarListReader {
+    /* ============ Errors ============ */
+    error InvalidRegistrarAddress();
+
+    /* ============ View/Pure Functions ============ */
+
+    /**
+     * @notice Returns whether `list` contains `account`.
+     * @dev This uses the standard list functionality of the Registrar contract and is included here for convenience.
+     * @param  list    The key for some list.
+     * @param  account The address of some account.
+     * @return Whether `list` contains `account`.
+     */
+    function listContains(bytes32 list, address account) external view returns (bool);
+
+    /**
+     * @notice Returns whether `list` contains all specified accounts.
+     * @dev This uses the standard list functionality of the Registrar contract and is included here for convenience.
+     * @param  list     The key for some list.
+     * @param  accounts An array of addressed of some accounts.
+     * @return Whether `list` contains all specified accounts.
+     */
+    function listContains(bytes32 list, address[] calldata accounts) external view returns (bool);
+
+    /**
+     * @notice Returns whether `list` contains `account`.
+     * @dev bytes32 lists are not natively supported by the Registrar contract.
+     *      This implementation assumes that the values are added to the Registrar
+     *      using the generic `setKey` function where the key provided is calculated
+     *      as `keccak256(abi.encodePacked(list, account))` and the value is treated
+     *      as a boolean (i.e. `bytes32(uint256(1))` for true and `bytes32(0)` for false).
+     * @param  list    The key for some list.
+     * @param  account The address of some account.
+     * @return Whether `list` contains `account`.
+     */
+    function listContains(bytes32 list, bytes32 account) external view returns (bool);
+
+    /**
+     * @notice Returns whether `list` contains all specified accounts.
+     * @dev bytes32 lists are not natively supported by the Registrar contract.
+     *      This implementation assumes that the values are added to the Registrar
+     *      using the generic `setKey` function where the key provided is calculated
+     *      as `keccak256(abi.encodePacked(list, account))` and the value is treated
+     *      as a boolean (i.e. `bytes32(uint256(1))` for true and `bytes32(0)` for false).
+     * @param  list     The key for some list.
+     * @param  accounts An array of addressed of some accounts.
+     * @return Whether `list` contains all specified accounts.
+     */
+    function listContains(bytes32 list, bytes32[] calldata accounts) external view returns (bool);
+}
